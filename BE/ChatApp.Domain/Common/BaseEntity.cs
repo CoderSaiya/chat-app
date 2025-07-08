58f0aa4d4e77dbd2c5f00913e19e476a31ab03b2
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-08 13:45:13 +0700

    define base entity

---
+++ BE/ChatApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Domain.Common;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+}
