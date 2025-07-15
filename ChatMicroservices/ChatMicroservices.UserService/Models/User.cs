7aa3b87d9f25b7139418724bc847785150ec7014
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-15 17:01:18 +0700

    define user and profile models

---
+++ ChatMicroservices/ChatMicroservices.UserService/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ChatMicroservice.Shared.Common;
+
+namespace UserService.Models;
+
+public abstract class User : BaseEntity
+{
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string? Email { get; set; }
+    
+    public Profile Profile { get; set; } = null!;
+}
