06428326354a1111c021512655e7172a0b69073e
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-15 17:06:18 +0700

    implement UserDbContext and configure entity mappings

---
+++ ChatMicroservices/ChatMicroservices.UserService/Data/UserDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data;
+
+public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Profile> Profiles => Set<Profile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+
+            entity.Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            entity.Property(u => u.Email)
+                .HasMaxLength(255);
+
+            entity.OwnsOne(u => u.Profile, profile =>
+            {
+                profile.Property(p => p.FullName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                profile.Property(p => p.AvatarUrl)
+                    .HasMaxLength(500);
+
+                profile.Property(p => p.DateOfBirth);
+                profile.Property(p => p.Bio)
+                    .HasMaxLength(1000);
+            });
+        });
+    }
+}
