f3f676d0183f48dccc6c23ecb4301a8bb5bbc7b9
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-18 10:44:33 +0700

    extract base Filter class for shared paging and sorting logic

---
+++ ChatMicroservices/ChatMicroservice.Shared/Common/Filter.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatMicroservice.Shared.Common;
+
+public class Filter
+{
+    public string? SortBy { get; set; }
+    public bool SortDescending { get; set; } = false;
+    public int? Skip { get; set; }
+    public int? Take { get; set; }
+}
