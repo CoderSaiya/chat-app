d62262a0aa052df750d883676dc1f9e867b86090
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-19 20:34:04 +0700

    add RabbitMqSettings configuration

---
+++ ChatMicroservices/ChatMicroservice.Shared/Common/RabbitMqSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatMicroservice.Shared.Common;
+
+public class RabbitMqSettings
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string QueueName { get; set; } = null!;
+}
