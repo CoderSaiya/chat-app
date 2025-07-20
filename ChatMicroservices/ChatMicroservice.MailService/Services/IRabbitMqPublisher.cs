fac57edfd119be87816204bb4857fadb78503c4b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-20 16:18:22 +0700

    implement RabbitMqPublisher to enqueue email messages

---
+++ ChatMicroservices/ChatMicroservice.MailService/Services/IRabbitMqPublisher.cs
@@ -0,0 +1,8 @@
+﻿using ChatMicroservice.MailService.Models;
+
+namespace ChatMicroservice.MailService.Services;
+
+public interface IRabbitMqPublisher
+{
+    void EnqueueMail(EmailMessage message);
+}
---
+++ ChatMicroservices/ChatMicroservice.MailService/Services/RabbitMqPublisher.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.Json;
+using ChatMicroservice.MailService.Models;
+using ChatMicroservice.Shared.Common;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace ChatMicroservice.MailService.Services;
+
+public class RabbitMqPublisher(IOptions<RabbitMqSettings> settings) : IRabbitMqPublisher
+{
+    private readonly RabbitMqSettings _settings = settings.Value;
+    
+    public void EnqueueMail(EmailMessage message)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _settings.Host,
+            Port = _settings.Port,
+            UserName = _settings.Username,
+            Password = _settings.Password
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: _settings.QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        
+        var json = JsonSerializer.Serialize(message);
+        var body = Encoding.UTF8.GetBytes(json);
+
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        channel.BasicPublish(exchange: "", routingKey: _settings.QueueName, basicProperties: properties, body: body);
+    }
+}
