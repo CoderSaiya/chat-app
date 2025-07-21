6c5c76ce6a003824b085e9ef60b0b9de5936f9cd
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-21 09:18:40 +0700

    define role for each user

--- ChatMicroservices/ChatMicroservices.UserService/Models/User.cs
+++ ChatMicroservices/ChatMicroservices.UserService/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using ChatMicroservice.Shared.Common;
+using UserService.Models.ValueObjects;
 
 namespace UserService.Models;
 
@@ -8,6 +9,7 @@ public abstract class User : BaseEntity
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string? Email { get; set; }
+    public UserRole Role { get; set; } = UserRole.Client;
     
     public Profile Profile { get; set; } = null!;
 }
