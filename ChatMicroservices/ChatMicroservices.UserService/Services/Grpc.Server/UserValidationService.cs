2b6f3157a64fb673dc20d4bae04be9a6b916d15b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-23 15:57:34 +0700

    implement logic for gRPC server

---
+++ ChatMicroservices/ChatMicroservices.UserService/Services/Grpc.Server/UserValidationService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using UserService.Grpc;
+
+namespace UserService.Services.Grpc.Server;
+
+public class UserValidationService(IUserService userService) : UserValidation.UserValidationBase
+{
+    public override async Task<UserResponse> Validate(LoginRequest request, ServerCallContext context)
+    {
+        var user = await userService.GetValidUser(request.Username, request.Password);
+        if (user == null)
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid credentials"));
+
+        return new UserResponse
+        {
+            Id = user.Id.ToString(),
+            Username = user.Username,
+            Role = user.Role.ToString()
+        };
+    }
+}
