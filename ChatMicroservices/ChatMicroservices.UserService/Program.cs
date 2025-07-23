224ea387da85acb60bf56a6bc9e661c1906d1cfa
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-23 12:55:07 +0700

    register MVC controllers

--- ChatMicroservices/ChatMicroservices.UserService/Program.cs
+++ ChatMicroservices/ChatMicroservices.UserService/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<UserDbContext>(options =>
         options.UseSqlServer(
             connectionString: builder.Configuration["ConnectionStrings:DefaultConnection"],
