11e5626cf26d55fce0deb55f5d04f48ddca81770
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-23 19:57:41 +0700

    define refresh token model

---
+++ ChatMicroservices/ChatMicroservices.AuthService/Models/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ChatMicroservice.Shared.Common;
+
+namespace AuthService.Models;
+
+public class RefreshToken : BaseEntity
+{
+    [Required]
+    public required Guid UserId { get; set; }
+    [Required]
+    public required string Token { get; set; } = null!;
+    [Required]
+    public required DateTime ExpiryDate  { get; set; }
+    public bool IsUsed { get; set; } = false;
+    public bool IsRevoked { get; set; } = false;
+}
