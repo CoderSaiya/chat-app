73fa70cc7ecf4c5f493d381ddafa26247773a411
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-23 19:33:31 +0700

    implement RefreshDbContext

---
+++ ChatMicroservices/ChatMicroservices.AuthService/Data/RefreshDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data;
+
+public class RefreshDbContext(DbContextOptions<RefreshDbContext> options) : DbContext(options)
+{
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Constrains
+
+        // Relations
+        
+    }
+}
