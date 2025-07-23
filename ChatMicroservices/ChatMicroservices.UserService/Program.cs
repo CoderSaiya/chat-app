b58630147823d4180069a42bd2b91820d3af2239
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-23 12:53:23 +0700

    configure EF Core Dbcontext

--- ChatMicroservices/ChatMicroservices.UserService/Program.cs
+++ ChatMicroservices/ChatMicroservices.UserService/Program.cs
@@ -1,8 +1,26 @@
+using ChatMicroservice.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+        options.UseSqlServer(
+            connectionString: builder.Configuration["ConnectionStrings:DefaultConnection"],
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            }),
+    contextLifetime: ServiceLifetime.Scoped,
+    optionsLifetime: ServiceLifetime.Singleton);
 
 var app = builder.Build();
 
