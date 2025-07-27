dd6f238b12437072135cf6886bcc36b5d8b748f3
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-27 11:19:31 +0700

    implement profile repo extended from generic repo

---
+++ ChatMicroservices/ChatMicroservices.UserService/Data/Repositories/IProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using ChatMicroservice.Shared.Persistence.Repositories;
+using UserService.Models;
+
+namespace UserService.Data.Repositories;
+
+public interface IProfileRepository : IGenericRepository<Profile>
+{
+    
+}
---
+++ ChatMicroservices/ChatMicroservices.UserService/Data/Repositories/ProfileRepository.cs
@@ -0,0 +1,9 @@
+﻿using ChatMicroservice.Shared.Persistence.Repositories;
+using UserService.Models;
+
+namespace UserService.Data.Repositories;
+
+public class ProfileRepository(UserDbContext context) : GenericRepository<Profile>(context), IProfileRepository
+{
+    
+}
