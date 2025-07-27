ed8d283da2d5e277d35c3ce757f4dd5538dedc50
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-07-27 12:32:39 +0700

    implement user creation logic

--- ChatMicroservices/ChatMicroservices.UserService/Services/IUserService.cs
+++ ChatMicroservices/ChatMicroservices.UserService/Services/IUserService.cs
@@ -5,4 +5,5 @@ namespace UserService.Services;
 public interface IUserService
 {
     Task<User?> GetValidUser(string username, string password);
+    Task<User?> Create(string username, string password, string role);
 }
--- ChatMicroservices/ChatMicroservices.UserService/Services/UserService.cs
+++ ChatMicroservices/ChatMicroservices.UserService/Services/UserService.cs
@@ -1,11 +1,38 @@
-﻿using UserService.Models;
+﻿using UserService.Data.Repositories;
+using UserService.Models;
+using UserService.Models.ValueObjects;
 
 namespace UserService.Services;
 
-public class UserService : IUserService
+public class UserService(IUserRepository userRepository) : IUserService
 {
     public async Task<User?> GetValidUser(string username, string password)
     {
-        throw new NotImplementedException();
+        var user = await userRepository.GetByUsername(username);
+        
+        if (user is null || user.Password != password) 
+            throw new Exception("Invalid username or password");
+        
+        return user;
+    }
+
+    public async Task<User?> Create(string username, string password, string role)
+    {
+        var user = await userRepository.GetByUsername(username);
+        if (user is not null)
+            throw new Exception("Username already exists");
+        
+        if (!Enum.TryParse(role, ignoreCase: true, out UserRole userRole))
+            throw new Exception($"Invalid role: {role}");
+
+        var newUser = new User
+        {
+            Username = username,
+            Password = password,
+            Role = userRole,
+        };
+        await userRepository.AddAsync(newUser);
+        
+        return newUser;
     }
 }
